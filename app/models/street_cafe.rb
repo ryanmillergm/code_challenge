ac311b4741a8e3998cb1aa696d4a444940edd35b
Ryan Miller <ryanmillergm@gmail.com>
2020-04-29 12:39:08 -0600

    Refactors street cafe model validations

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -1,6 +1,7 @@
 class StreetCafe < ApplicationRecord
-  validates_presence_of :restaurant_name
-  validates_presence_of :street_address
-  validates_presence_of :post_code
-  validates_presence_of :number_of_chairs
+  validates_presence_of :restaurant_name,
+                        :street_address,
+                        :post_code,
+                        :number_of_chairs
+
 end
