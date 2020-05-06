256fb086e4e4920d350014878e2f8423891cc0f6
Ryan Miller <ryanmillergm@gmail.com>
2020-05-06 12:49:08 -0600

    Updates imprts csv spec

--- spec/tasks/imports_csv_spec.rb
+++ spec/tasks/imports_csv_spec.rb
@@ -3,13 +3,14 @@ require 'rake'
 
 Rails.application.load_tasks
 
-RSpec.describe 'import:street_cafes' do
-  it 'imports street cafes from csv file' do
-    binding.pry
-    expect(StreetCafe.count).to eq(0)
+RSpec.describe "Rake Tasks" do
+  describe 'import:street_cafes' do
+    it 'imports street cafes from csv file' do
+      expect(StreetCafe.count).to eq(0)
 
-    Rake::Task['import:street_cafes'].execute
+      Rake::Task['import:street_cafes'].execute
 
-    expect(StreetCafe.count).to eq(73)
+      expect(StreetCafe.count).to eq(73)
+    end
   end
 end
