8ad96f7038300124177cb75a5fba6721f0e5e91b
Ryan Miller <ryanmillergm@gmail.com>
2020-05-07 15:04:34 -0600

    Refactors cafe categorizer task to CafeCategorizer

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -4,26 +4,4 @@ class StreetCafe < ApplicationRecord
                         :post_code,
                         :number_of_chairs
 
-
-  def categorize_by_number_of_chairs(prefix)
-    if number_of_chairs < 10
-      update!(category: "#{prefix} small" )
-    elsif number_of_chairs >= 10 && number_of_chairs < 100
-      update!(category: "#{prefix} medium" )
-    else
-      update!(category: "#{prefix} large" )
-    end
-  end
-
-  def categorize_by_chair_percentile(prefix, cafes_ordered_by_chairs_in_ls2)
-    cafe = self
-    position = cafes_ordered_by_chairs_in_ls2.index(cafe) + 1
-    percentile  = (position.to_f / cafes_ordered_by_chairs_in_ls2.count) * 100
-    
-    if percentile < 50
-      update!(category: "#{prefix} small" )
-    elsif percentile >= 50
-      update!(category: "#{prefix} large" )
-    end
-  end
 end
