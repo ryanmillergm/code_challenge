901cf3de3a45c079cd5bedf51b68a76084971c66
Ryan Miller <ryanmillergm@gmail.com>
2020-05-07 15:49:30 -0600

    Updates cafes ordered by chairs

--- app/models/cafe_categorizer.rb
+++ app/models/cafe_categorizer.rb
@@ -3,18 +3,18 @@ class CafeCategorizer
     @cafe = cafe
   end
 
-  def categorize_cafe(cafes_ordered_by_chairs_in_ls2)
+  def categorize_cafe(cafes_ordered_by_chairs)
     prefix = @cafe.post_code.split(' ').first.downcase
 
     case prefix
     when 'ls1'
       categorize_by_number_of_chairs(prefix)
     when 'ls2'
-      categorize_by_chair_percentile(prefix, cafes_ordered_by_chairs_in_ls2)
+      categorize_by_chair_percentile(prefix, cafes_ordered_by_chairs)
     else
       @cafe.category = "other"
     end
-    
+
     @cafe.save
   end
 
@@ -30,9 +30,9 @@ class CafeCategorizer
     end
   end
 
-  def categorize_by_chair_percentile(prefix, cafes_ordered_by_chairs_in_ls2)
-    position = cafes_ordered_by_chairs_in_ls2.index(@cafe) + 1
-    percentile  = (position.to_f / cafes_ordered_by_chairs_in_ls2.count) * 100
+  def categorize_by_chair_percentile(prefix, cafes_ordered_by_chairs)
+    position = cafes_ordered_by_chairs.index(@cafe) + 1
+    percentile  = (position.to_f / cafes_ordered_by_chairs.count) * 100
 
     if percentile < 50
       @cafe.category = "#{prefix} small"
