544b84c4840792846a1811a3d7b02a4ab8b90908
Ryan Miller <ryanmillergm@gmail.com>
2020-05-08 14:35:58 -0600

    Updates cafes_by_size method to take in multiple arguments

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -4,8 +4,15 @@ class StreetCafe < ApplicationRecord
                         :post_code,
                         :number_of_chairs
 
-  def self.cafes_by_size(size)
-    where("category LIKE ?", "%#{size}")
+  def self.cafes_by_size(*size)
+    if size.count == 1
+      where("category LIKE ?", "%#{size}")
+    elsif size.count == 2
+      where("category LIKE ?", "%#{size[0]}").or(where("category LIKE ?", "%#{size[1]}"))
+      # where("category LIKE '%#{size[0]}' OR category LIKE '%#{size[1]}'")
+    else
+      raise ArgumentError, "Wrong number of arguments. Expecting 1-2."
+    end
   end
 
 end
